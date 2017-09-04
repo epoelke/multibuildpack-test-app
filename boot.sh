c309b972d6709db326f4a885cd76dc5c581ad813
Eric Poelke <eric.poelke@philips.com>
2017-09-04 08:17:49 -0700

    Remove custom boot.sh script and use one-liner in manifest.

--- boot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-$APP_ROOT/start_logging.sh
-uwsgi --master --processes 4 --threads 2 --http /tmp/uwsgi.sock --wsgi-file public/app.py &
-nginx -p $APP_ROOT/nginx -c $APP_ROOT/nginx/conf/nginx.conf
